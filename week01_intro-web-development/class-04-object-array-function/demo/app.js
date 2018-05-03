51c0340757d3ae0d47a744647104acd0c0261998
martypdx <marty@martypdx.com>
2018-05-03 11:26:17 -0700

    setup and pseudo-code

---
+++ week01_intro-web-development/class-04-object-array-function/demo/app.js
@@ -0,0 +1,23 @@
+
+
+function makePairs() {
+    // takes no parameters
+
+    // loop 9 times
+
+        // randomly select first student
+        // randomly select second student
+        // write pair to dom
+
+    // no return value, dom is just updated
+}
+
+function randomStudent() {
+    // takes no parameters
+
+    // get a random integer
+    // gets that student from the array by the integer as index
+    // remove that student from the array
+
+    // return the randomly selected student
+}
---
+++ week01_intro-web-development/class-04-object-array-function/demo/students.js
@@ -0,0 +1,22 @@
+/* exported students */
+
+var students = [
+    'Chrisgolden77'
+    'easton-john'
+    'arthurjen'
+    'KLPainter'
+    'RyLuras'
+    'AttilaTheHen'
+    'rbtprograms'
+    'markalope'
+    'rehmsy'
+    'MariahAdams'
+    'soundparticle'
+    'carriecarter'
+    'WithakErik'
+    'zachar13y'
+    'daisywebb31'
+    'robinhood182'
+    'ryankh87'
+    'carmenvramos'
+]; 
